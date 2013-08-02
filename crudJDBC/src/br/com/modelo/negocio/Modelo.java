10c8ac93e6811b2bb57562e8d38011b16f4aaa1c
leticia_macena <leticia_macena@10.1.49.192>
2013-08-02 15:41:35 -0300

    criando a classe   modelo

---
+++ crudJDBC/src/br/com/modelo/negocio/Modelo.java
@@ -0,0 +1,30 @@
+package br.com.modelo.negocio;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leticia_macena
+ */
+public class Modelo {
+    int id;
+    String descricao;
+    int potencia;
+     Marca maca;
+    
+     
+     
+     public Modelo(){
+         
+     }
+     /**
+      * return the id
+      */
+     
+     public int getId(){
+         return id;
+     }
+}
